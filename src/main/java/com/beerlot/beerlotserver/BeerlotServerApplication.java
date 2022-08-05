ace07d9fd8d5a1e2274de86b552d4001aa337b43
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-08-05 22:38:39 +0200

    env: Import spring project

---
+++ src/main/java/com/beerlot/beerlotserver/BeerlotServerApplication.java
@@ -0,0 +1,13 @@
+package com.beerlot.beerlotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerlotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerlotServerApplication.class, args);
+	}
+
+}
