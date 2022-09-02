07a64f20d5ade0d5ae24cfb2fd58fc6f34beaf65
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-02 14:55:38 +0200

    feat: Add SpringBootApplication annotation to CoreApplication

---
+++ core/src/main/java/com/beerlot/core/CoreApplication.java
@@ -0,0 +1,10 @@
+package com.beerlot.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+    }
+}
