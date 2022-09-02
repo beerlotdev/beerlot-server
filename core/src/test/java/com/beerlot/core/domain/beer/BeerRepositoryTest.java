3854e2d63f6b471ecf22e79d9b29c35e35dd7bb8
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-02 14:55:12 +0200

    refactor: Create BaseRepositoryTest to manage entity save

--- core/src/test/java/com/beerlot/core/domain/beer/BeerRepositoryTest.java
+++ core/src/test/java/com/beerlot/core/domain/beer/BeerRepositoryTest.java
@@ -1,78 +1,57 @@
 package com.beerlot.core.domain.beer;
 
+import com.beerlot.core.BaseRepositoryTest;
 import com.beerlot.core.config.QueryDslConfig;
 import com.beerlot.core.domain.tag.Tag;
-import com.beerlot.core.domain.tag.TagRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @Import(QueryDslConfig.class)
-@SpringBootTest
-public class BeerCustomRepositoryTest {
-    @Autowired
-    BeerCustomRepository beerCustomRepository;
+public class BeerRepositoryTest extends BaseRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
-    @Autowired
-    TagRepository tagRepository;
-    @Autowired
-    BeerTagRepository beerTagRepository;
-    @Autowired
-    CategoryRepository categoryRepository;
 
-    Beer beer1;
+    Beer beer;
     Category ctAmericanBlondeAle;
     Tag tgGinger;
-    BeerTag beerTag1;
+    BeerTag beerTag;
 
     @BeforeEach
     public void setUp() {
-        ctAmericanBlondeAle = Category.builder()
+        ctAmericanBlondeAle = save(Category.builder()
                 .nameKo("아메리칸 블론드 에일")
                 .nameEn("American Blonde Ale")
                 .description("This is American Blonde Ale")
                 .parent(null)
-                .build();
+                .build());
 
-        categoryRepository.save(ctAmericanBlondeAle);
-
-        tgGinger = Tag.builder()
+        tgGinger = save(Tag.builder()
                 .nameKo("생강")
                 .nameEn("Ginger")
                 .description("This is Ginger.")
-                .build();
+                .build());
 
-        tagRepository.save(tgGinger);
-
-        beer1 = Beer.builder()
+        beer = save(Beer.builder()
                 .nameKo("빅 웨이브")
                 .nameEn("Big Wave")
                 .description("This is Bigwave.")
                 .volume(4.4f)
                 .origin(Country.US)
                 .category(ctAmericanBlondeAle)
-                .build();
+                .build());
 
-        beerRepository.save(beer1);
-
-        beerTag1 = BeerTag.builder()
-                .beer(beer1)
+        beerTag = merge(BeerTag.builder()
+                .beer(beer)
                 .tag(tgGinger)
-                .build();
-
-        beerTagRepository.save(beerTag1);
-
+                .build());
     }
 
     @Test
@@ -83,7 +62,7 @@ public class BeerCustomRepositoryTest {
 
     @Test
     public void findByKeywordAndTags() {
-        List<Beer> beers = beerCustomRepository.findByKeywordAndTags(null, List.of(tgGinger));
+        List<Beer> beers = beerRepository.findByKeywordAndTags(null, List.of(tgGinger));
         Assertions.assertEquals(1, beers.size());
     }
 }
