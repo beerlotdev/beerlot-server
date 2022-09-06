1cc4b2b110a22b5ab99e2fb49c5bcd70bb8c0f9b
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-06 10:29:40 +0200

    test: Add testcases for findBySearch

--- beerlot-core/src/test/java/com/beerlot/core/domain/beer/repository/BeerRepositoryTest.java
+++ beerlot-core/src/test/java/com/beerlot/core/domain/beer/repository/BeerRepositoryTest.java
@@ -10,6 +10,7 @@ import com.beerlot.core.domain.tag.BeerTag;
 import com.beerlot.core.domain.tag.Tag;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
@@ -17,56 +18,153 @@ import org.springframework.context.annotation.Import;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Import(QueryDslConfig.class)
 public class BeerRepositoryTest extends BaseRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
 
-    Beer beer;
-    Category ctAmericanBlondeAle;
-    Tag tgGinger;
-    BeerTag beerTag;
+    Beer beerBigwave;
+    Beer beerMaineBeerDinner;
+    Category categoryAmericanBlondeAle;
+    Category categoryImperialIPA;
+    Tag tagCitra;
+    BeerTag beerTagBigwaveCitra;
+    BeerTag beerTagMaineBeerDinnerCitra;
 
     @BeforeEach
     public void setUp() {
-        ctAmericanBlondeAle = save(Category.builder()
+        categoryAmericanBlondeAle = save(Category.builder()
                 .nameKo("아메리칸 블론드 에일")
                 .nameEn("American Blonde Ale")
-                .description("This is American Blonde Ale")
+                .description("American Blonde Ale is blah blah.")
                 .parent(null)
                 .build());
 
-        tgGinger = save(Tag.builder()
-                .nameKo("생강")
-                .nameEn("Ginger")
-                .description("This is Ginger.")
+        categoryImperialIPA = save(Category.builder()
+                .nameKo("임페리얼 아이피에이")
+                .nameEn("Imperial IPA")
+                .description("Imperial IPA is bla bla.")
+                .parent(null)
                 .build());
 
-        beer = save(Beer.builder()
+        tagCitra = save(Tag.builder()
+                .nameKo("시트라")
+                .nameEn("Citra")
+                .description("Citra is blah blah.")
+                .build());
+
+        beerBigwave = save(Beer.builder()
                 .nameKo("빅 웨이브")
-                .nameEn("Big Wave")
-                .description("This is Bigwave.")
+                .nameEn("Bigwave")
+                .description("Bigwave is blah blah.")
                 .volume(4.4f)
                 .origin(Country.US)
-                .category(ctAmericanBlondeAle)
+                .category(categoryAmericanBlondeAle)
+                .imageUrl("https://beerlot.com/image_url")
                 .build());
 
-        beerTag = merge(BeerTag.builder()
-                .beer(beer)
-                .tag(tgGinger)
+        beerMaineBeerDinner = save(Beer.builder()
+                .nameKo("마이네 비어 디너")
+                .nameEn("Maine Beer Dinner")
+                .description("Maine Beer Dinner is blah blah.")
+                .volume(8.2f)
+                .origin(Country.US)
+                .category(categoryImperialIPA)
+                .imageUrl("https://beerlot.com/image_url")
                 .build());
+
+        beerTagBigwaveCitra = merge(BeerTag.builder()
+                .beer(beerBigwave)
+                .tag(tagCitra)
+                .build());
+
+        beerTagMaineBeerDinnerCitra = merge(BeerTag.builder()
+                .beer(beerMaineBeerDinner)
+                .tag(tagCitra)
+                .build());
+
     }
 
-    @Test
-    public void findById() {
-        Optional<Beer> beer = beerRepository.findById(1L);
-        Assertions.assertEquals(1L, beer.get().getId());
+    @Nested
+    class FindById {
+
+        @Test
+        public void findById() {
+            Optional<Beer> beer = beerRepository.findById(beerBigwave.getId());
+            Assertions.assertTrue(beer.isPresent());
+        }
     }
 
-    @Test
-    public void findByKeywordAndTags() {
-        List<BeerResDto> beers = beerRepository.findByKeywordAndTags(null, List.of(tgGinger));
-        Assertions.assertEquals(1, beers.size());
+    @Nested
+    class FindBySearch {
+
+        @Test
+        public void givenOneCategory() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, List.of(categoryImperialIPA), null, null);
+            assertEquals(1, beers.size());
+        }
+
+        @Test
+        public void givenTwoCategories() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, List.of(categoryImperialIPA, categoryAmericanBlondeAle), null, null);
+            assertEquals(2, beers.size());
+        }
+
+        @Test
+        public void givenOneCountry() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, List.of(Country.US), null);
+            assertEquals(2, beers.size());
+        }
+
+        @Test
+        public void givenCountryHasNoBeers() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, List.of(Country.AD), null);
+            assertEquals(0, beers.size());
+        }
+
+        @Test
+        public void givenKeywordForBeerDescription() {
+            List<BeerResDto> beers = beerRepository.findBySearch("Dinner", null, null, null);
+            assertEquals(1, beers.size());
+        }
+
+        @Test
+        public void givenKeywordForTagDescription() {
+            List<BeerResDto> beers = beerRepository.findBySearch("Citra", null, null, null);
+            assertEquals(2, beers.size());
+        }
+
+        @Test
+        public void givenKeywordForCategoryDescription() {
+            List<BeerResDto> beers = beerRepository.findBySearch("IPA", null, null, null);
+            assertEquals(1, beers.size());
+        }
+
+        @Test
+        public void givenOneVolume() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, null, List.of(4));
+            assertEquals(1, beers.size());
+        }
+
+        @Test
+        public void givenTwoVolumes() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, null, List.of(4, 8));
+            assertEquals(2, beers.size());
+        }
+
+        @Test
+        public void givenVolumeHasNoBeer() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, null, List.of(5));
+            assertEquals(0, beers.size());
+        }
+
+        @Test
+        public void givenNoSearchCondition() {
+            List<BeerResDto> beers = beerRepository.findBySearch(null, null, null, null);
+            assertEquals(2, beers.size());
+        }
     }
 }
 
