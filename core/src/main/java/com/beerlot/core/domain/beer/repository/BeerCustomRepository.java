3aad8048df38115ba2dbf1bd628f3ac0b9102009
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-07 15:19:24 +0200

    chore: Remove unused directory

--- core/src/main/java/com/beerlot/core/domain/beer/repository/BeerCustomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.beerlot.core.domain.beer;
-
-import com.beerlot.core.domain.tag.Tag;
-import java.util.List;
-
-public interface BeerCustomRepository {
-    List<BeerCustomRepositoryImpl.BeerResDto> findByKeywordAndTags(String keyword, List<Tag> tags);
-}
