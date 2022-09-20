5bb3273994ca26222dde257381c729f93332ac5f
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-20 22:21:54 +0200

    fix: Remove RequestMapping from beer controller

--- beerlot-core/src/main/java/com/beerlot/core/domain/beer/controller/BeerController.java
+++ beerlot-core/src/main/java/com/beerlot/core/domain/beer/controller/BeerController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/beers")
 public class BeerController implements BeerApi {
 
     @Autowired
