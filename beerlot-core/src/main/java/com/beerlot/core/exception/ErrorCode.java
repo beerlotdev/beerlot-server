06648b866ef11cb43f33e9162354522cea95146f
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-27 01:12:54 +0200

    feat: Implement Review Like APIs

--- beerlot-core/src/main/java/com/beerlot/core/exception/ErrorCode.java
+++ beerlot-core/src/main/java/com/beerlot/core/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
     BEER_NOT_FOUND(HttpStatus.NOT_FOUND, "B001", "Beer does not exist."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "R001", "Review does not exist."),
     BEER_LIKE_NOT_FOUND(HttpStatus.OK, "BL001", "User has not liked Beer."),
-    BEER_LIKE_CONFLICT(HttpStatus.OK, "BL002", "User already liked Beer.");
+    BEER_LIKE_CONFLICT(HttpStatus.OK, "BL002", "User already liked Beer."),
+    REVIEW_LIKE_NOT_FOUND(HttpStatus.OK, "RL001", "User has not liked Review."),
+    REVIEW_LIKE_CONFLICT(HttpStatus.OK, "RL002", "User already liked Review.");
 
     private HttpStatus status;
     private String code;
