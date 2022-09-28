31ae4514516979e01a24cfb409d5b14f9ac5970b
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-28 13:31:40 +0200

    fix: Configure web security

---
+++ beerlot-core/src/main/java/com/beerlot/core/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.beerlot.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/api/v1").permitAll()
+                );
+
+        return http.build();
+    }
+
+}
