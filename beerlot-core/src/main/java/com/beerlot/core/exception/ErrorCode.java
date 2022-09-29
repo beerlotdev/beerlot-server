7453e6dc3e0440bf631919f6b11adfa02aed3ac4
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-09-29 21:48:51 +0200

    fix: Fix response code for likes behaviour

--- beerlot-core/src/main/java/com/beerlot/core/exception/ErrorCode.java
+++ beerlot-core/src/main/java/com/beerlot/core/exception/ErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     BEER_NOT_FOUND(HttpStatus.NOT_FOUND, "B001", "Beer does not exist."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "R001", "Review does not exist."),
-    BEER_LIKE_NOT_FOUND(HttpStatus.OK, "BL001", "User has not liked Beer."),
-    BEER_LIKE_CONFLICT(HttpStatus.OK, "BL002", "User already liked Beer."),
-    REVIEW_LIKE_NOT_FOUND(HttpStatus.OK, "RL001", "User has not liked Review."),
-    REVIEW_LIKE_CONFLICT(HttpStatus.OK, "RL002", "User already liked Review.");
+    BEER_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "BL001", "User has not liked Beer."),
+    BEER_LIKE_CONFLICT(HttpStatus.CONFLICT, "BL002", "User already liked Beer."),
+    REVIEW_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "RL001", "User has not liked Review."),
+    REVIEW_LIKE_CONFLICT(HttpStatus.CONFLICT, "RL002", "User already liked Review.");
 
     private HttpStatus status;
     private String code;
