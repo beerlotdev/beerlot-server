b6ba475973e65814a74d5c30328a3d1db430fa93
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-11-03 04:56:50 +0100

    feat: Implement findTop10Beers

--- beerlot-core/src/main/java/com/beerlot/core/domain/beer/repository/BeerRepository.java
+++ beerlot-core/src/main/java/com/beerlot/core/domain/beer/repository/BeerRepository.java
@@ -4,9 +4,11 @@ import com.beerlot.core.domain.beer.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BeerRepository extends JpaRepository<Beer, Long>, BeerCustomRepository {
     Optional<Beer> findById(Long id);
+    List<Beer> findTop10ByOrderByLikeCountDesc();
 }
