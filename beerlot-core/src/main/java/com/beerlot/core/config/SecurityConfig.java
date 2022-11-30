b8799092e777c6b3df2c6def9a3a0b7b3986be56
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-11-30 21:35:07 +0100

    chore: Change the name of SecurityConfig

--- beerlot-core/src/main/java/com/beerlot/core/config/SecurityConfig.java
+++ beerlot-core/src/main/java/com/beerlot/core/config/SecurityConfig.java
@@ -2,30 +2,28 @@ package com.beerlot.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig{
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //http.addFilterAfter(jwtAuthenticationFilter, LogoutFilter.class);
         http
                 .cors()
                 .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v1/**").permitAll()
-                .and()
-                .csrf().disable();
+                //.and()
+                //.oauth2ResourceServer().jwt()
+                .and();
 
         return http.build();
     }
+
 }
