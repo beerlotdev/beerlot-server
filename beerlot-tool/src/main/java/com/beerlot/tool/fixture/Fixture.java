92c8c995e7bb9d636d8cb87e2ed962e73e0837c2
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-01-06 20:04:03 +0100

    test: Fix auth controller signup tests

--- beerlot-tool/src/main/java/com/beerlot/tool/fixture/Fixture.java
+++ beerlot-tool/src/main/java/com/beerlot/tool/fixture/Fixture.java
@@ -27,6 +27,17 @@ public class Fixture {
                 .provider(ProviderType.GOOGLE)
                 .build();
     }
+
+    public static Member createGuestMember() {
+        return Member.builder()
+                .oauthId("123456789")
+                .email("email@email.com")
+                .imageUrl("https://beerlot.com/image_url")
+                .username("김비어")
+                .roles(Set.of(RoleType.GUEST))
+                .provider(ProviderType.GOOGLE)
+                .build();
+    }
     /* ===== Beer ===== */
     public static BeerInternational createBeerInternational() {
         return BeerInternational.builder()
