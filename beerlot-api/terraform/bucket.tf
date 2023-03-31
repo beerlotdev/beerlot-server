ac72ef63e4298fec416f5a0a607358c0d6749785
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-03-31 16:59:00 +0200

    fix: Fix the bucket resource name

--- beerlot-api/terraform/bucket.tf
+++ beerlot-api/terraform/bucket.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "static-site" {
+resource "google_storage_bucket" "image_buckets" {
   for_each = toset(var.image_buckets)
   name          = "beerlot-images/${each.key}"
   project = var.project
@@ -6,4 +6,5 @@ resource "google_storage_bucket" "static-site" {
   force_destroy = true
 
   uniform_bucket_level_access = true
+
 }
