95c7b038d7cecb611628c50f59e73d8b2392936c
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-04-01 21:24:49 +0200

    feat: Create subfolders in bucket using terraform

--- beerlot-api/terraform/bucket.tf
+++ beerlot-api/terraform/bucket.tf
@@ -1,10 +1,19 @@
-resource "google_storage_bucket" "image_buckets" {
-  for_each = toset(var.image_buckets)
-  name          = "beerlot-images/${each.key}"
-  project = var.project
+resource "google_storage_bucket" "image_bucket" {
+  project       = var.project
   location      = var.region
+
+  name          = "images-${var.project}"
   force_destroy = true
-
   uniform_bucket_level_access = true
+}
 
+resource "google_storage_bucket" "image_bucket_subfolders" {
+  project       = var.project
+  location      = var.region
+
+  for_each = toset(var.image_buckets)
+  name          = "${each.key}/"
+  bucket        = google_storage_bucket.image_bucket.name
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
