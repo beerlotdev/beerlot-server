54ece3e7ea359c9b8c1cf7daaf756a6218c530de
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-04-18 22:34:59 +0200

    fix: Delete resource manager and enable it by cloud console manually

--- beerlot-api/terraform/api.tf
+++ beerlot-api/terraform/api.tf
@@ -8,17 +8,4 @@ resource "google_project_service" "apis" {
   project                    = var.project
   service                    = each.key
   disable_dependent_services = true
-  depends_on = [
-  google_project_service.resource_manager_api
-  ]
-}
-
-resource "google_project_service" "resource_manager_api" {
-  for_each = toset([
-    "cloudresourcemanager.googleapis.com",
-
-  ])
-  project                    = var.project
-  service                    = each.key
-  disable_dependent_services = true
 }
