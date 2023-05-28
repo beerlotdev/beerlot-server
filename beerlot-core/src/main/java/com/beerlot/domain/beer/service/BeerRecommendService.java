2e1fdfdb3d3910cd19e147ead097f9e4079c0db3
han-gyeong <hangyeong@outlook.com>
2023-05-28 16:14:52 +0900

    Feature: add Repository for Recommendation DataModel

--- beerlot-core/src/main/java/com/beerlot/domain/beer/service/BeerRecommendService.java
+++ beerlot-core/src/main/java/com/beerlot/domain/beer/service/BeerRecommendService.java
@@ -1,11 +1,12 @@
 package com.beerlot.domain.beer.service;
 
 import com.beerlot.domain.beer.dto.response.BeerResponse;
+import com.beerlot.domain.beer.repository.BeerRecommendRepository;
 import com.beerlot.domain.common.entity.LanguageType;
 import com.beerlot.domain.member.Member;
 import com.beerlot.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.apache.mahout.cf.taste.impl.model.jdbc.PostgreSQLJDBCDataModel;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
@@ -13,43 +14,23 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class BeerRecommendService {
-
-    /*
-    create view recommend (member_id, item_id, value) as
-    SELECT
-        member_id,
-        beer_id as item_id,
-        '5.0' as value
-    FROM
-        beer_like
-    UNION
-    SELECT
-        member_id,
-        beer_id,
-        rate as value
-    FROM
-        review;
-     */
-
-    private final DataSource dataSource;
 
     private final BeerService beerService;
 
     private final MemberService memberService;
 
+    private final BeerRecommendRepository beerRecommendRepository;
+
     public List<BeerResponse> recommend(String oauthId, LanguageType languageType, int amount) {
         Member foundMember = memberService.findMemberByOauthId(oauthId);
-
-        DataModel dataModel = new PostgreSQLJDBCDataModel(
-                dataSource, "recommend", "member_id", "item_id", "value", null
-        );
+        DataModel dataModel = beerRecommendRepository.getDataModel();
 
         List<BeerResponse> response = new ArrayList<>();
         try {
@@ -62,7 +43,7 @@ public class BeerRecommendService {
                 response.add(beerService.findBeerByIdAndLanguage(item.getItemID(), languageType));
             }
         } catch (Exception e) {
-
+            log.error("Beer Recommend Error : {}", e.getMessage());
         }
 
         return response;
