53a8b97da357d17914e7444834fe2898fe9b7723
han-gyeong <hangyeong@outlook.com>
2023-06-11 17:37:14 +0900

    Refactor: make DataModel refreshable periodically, extract table info into yml

--- beerlot-core/src/main/java/com/beerlot/domain/beer/repository/BeerRecommendRepositoryImpl.java
+++ beerlot-core/src/main/java/com/beerlot/domain/beer/repository/BeerRecommendRepositoryImpl.java
@@ -1,13 +1,20 @@
 package com.beerlot.domain.beer.repository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.jdbc.PostgreSQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class BeerRecommendRepositoryImpl implements BeerRecommendRepository {
@@ -28,30 +35,55 @@ public class BeerRecommendRepositoryImpl implements BeerRecommendRepository {
     FROM
         review;
      */
+    private static final int UPDATE_PERIOD_IN_MINUTE = 5;
 
-    private static final Long UPDATE_PERIOD = 5L;
+    @Value("${recommend.table}")
+    private String preferenceTable;
+
+    @Value("${recommend.user_id_column}")
+    private String userIdColumn;
+
+    @Value("${recommend.item_id_column}")
+    private String itemIdColumn;
+
+    @Value("${recommend.pref_column}")
+    private String preferenceColumn;
+
+    private final DataSource dataSource;
 
     private LocalDateTime updatedAt;
 
     private DataModel dataModel;
 
-    private final DataSource dataSource;
 
     @Override
-    public synchronized DataModel getDataModel() {
-        if (updatedAt.isBefore(LocalDateTime.now().minusMinutes(UPDATE_PERIOD))) {
+    public DataModel getDataModel() {
+        if (updatedAt.isBefore(LocalDateTime.now().minusMinutes(UPDATE_PERIOD_IN_MINUTE))) {
+            dataModel.refresh(null);
             updatedAt = LocalDateTime.now();
-
-            dataModel = new PostgreSQLJDBCDataModel(
-                    dataSource,
-                    "recommend",
-                    "member_id",
-                    "item_id",
-                    "value",
-                    null
-            );
         }
 
         return dataModel;
+    }
+
+    @PostConstruct
+    private void init() {
+        try {
+            dataModel = new ReloadFromJDBCDataModel(
+                    new PostgreSQLJDBCDataModel(
+                            dataSource,
+                            preferenceTable,
+                            userIdColumn,
+                            itemIdColumn,
+                            preferenceColumn,
+                            null
+                    )
+            );
+
+            updatedAt = LocalDateTime.now();
+        } catch (TasteException e) {
+            log.error("getDataModel Exception : {}", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
