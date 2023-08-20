659f2fc9dfbfff9dec12a449e964ae895260f883
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-08-20 13:07:10 +0200

    186: Find all review likes by member

--- beerlot-core/src/main/java/com/beerlot/domain/review/repository/ReviewLikeRepository.java
+++ beerlot-core/src/main/java/com/beerlot/domain/review/repository/ReviewLikeRepository.java
@@ -5,8 +5,11 @@ import com.beerlot.domain.review.ReviewLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, ReviewLikeId> {
     boolean existsByReview_IdAndMember_Id(Long reviewId, Long memberId);
     void deleteByReview_IdAndMember_Id(Long reviewId, Long memberId);
+    List<ReviewLike> findByMember_Id(Long memberId);
 }
--- beerlot-core/src/main/java/com/beerlot/domain/review/service/ReviewLikeService.java
+++ beerlot-core/src/main/java/com/beerlot/domain/review/service/ReviewLikeService.java
@@ -15,6 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -52,6 +56,13 @@ public class ReviewLikeService {
 
         reviewLikeRepository.deleteByReview_IdAndMember_Id(reviewId, member.getId());
         review.unlikeReview();
+    }
+
+    public List<Long> getReviewLikesByMember(String oauthId) {
+        Member member = memberService.findMemberByOauthId(oauthId);
+
+        List<ReviewLike> reviewLikes = reviewLikeRepository.findByMember_Id(member.getId());
+        return reviewLikes.stream().map(reviewLike -> reviewLike.getReview().getId()).toList();
     }
 
     @Transactional(readOnly = true)
