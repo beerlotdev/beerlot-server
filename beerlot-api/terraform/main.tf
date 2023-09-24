96ab1cce4316925c60ed9a55a549ab29521127ef
YOUJIN LEE <37237235+dev-ujin@users.noreply.github.com>
2023-09-24 13:57:39 +0200

    Revert "208: Make image bucket structure flat"
    This reverts commit d38f6ac56b91a0d59241fed0b83874504b7e0ef5.

--- beerlot-api/terraform/main.tf
+++
@@ -1,7 +0,0 @@
-module "image_buckets" {
-  source = "./modules/storage"
-  project = var.project
-  region = var.region
-  for_each = toset(var.image_buckets)
-  bucket_name = "${each.key}-images-${var.project}"
-}
--- beerlot-api/terraform/modules/storage/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "image_bucket" {
-  project  = var.project
-  location = var.region
-
-  name                        = var.bucket_name
-  uniform_bucket_level_access = true
-}
