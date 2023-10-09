53080de852bf95daaae31ced95eac6028f9724d9
YOUJIN LEE <youjin.lee.dev@gmail.com>
2023-10-09 21:59:55 +0200

    227: Set deletion protection to database instance

--- beerlot-api/terraform/sql.tf
+++ beerlot-api/terraform/sql.tf
@@ -6,6 +6,7 @@ resource "google_sql_database_instance" "beerlot_database_instance" {
   settings {
     tier = "db-f1-micro"
   }
+  deletion_protection = true
 }
 
 resource "google_sql_database" "beerlot_database" {
