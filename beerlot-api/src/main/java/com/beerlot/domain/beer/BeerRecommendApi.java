3546b7a9419493d79cf566a1c57e208b9d91892d
han-gyeong <hangyeong@outlook.com>
2024-08-18 14:22:12 +0900

    Fix: resolve conflicts error

--- beerlot-api/src/main/java/com/beerlot/domain/beer/BeerRecommendApi.java
+++ beerlot-api/src/main/java/com/beerlot/domain/beer/BeerRecommendApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RequestMapping("/api/v1/beers")
